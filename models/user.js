830af0a1ee50f7cfd6704c70e9b3fd3292e89cef
Traci <traci.face@gmail.com>
2021-05-30 19:44:31 -0400

    auth and user model added

---
+++ models/user.js
@@ -0,0 +1,54 @@
+// create imports
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+//set up object
+class User extends Model {
+    // sets the password check method
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+  }
+}
+
+// defines the columns of the user table
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8],
+      },
+    },
+  },
+  {
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      beforeUpdate: async (updatedUserData) => {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
+      },
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+  }
+);
+//sets as user to export
+module.exports = User;
