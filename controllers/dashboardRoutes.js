54da6cf6c94b39334025861b3a5286fe9bce389c
Traci <traci.face@gmail.com>
2021-05-31 16:48:55 -0400

    dashboard routes

---
+++ controllers/dashboardRoutes.js
@@ -0,0 +1,77 @@
+const router = require("express").Router();
+const sequelize = require("../config/connection");
+const { Post, User, Comment } = require("../models");
+
+router.get("/", (req, res) => {
+  Post.findAll({
+    where: {
+      user_id: req.session.user_id,
+    },
+    attributes: ["id", "title", "content", "date_created"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_content", "post_id", "user_id", "date_created"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      // serialize data before passing to template
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+      res.render("dashboard", { posts, loggedIn: true });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get("/edit/:id", (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "title", "content", "date_created"],
+    include: [
+      {
+        model: User,
+        attributes: ["username"],
+      },
+      {
+        model: Comment,
+        attributes: ["id", "comment_content", "post_id", "user_id", "date_created"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "Post not found" });
+        return;
+      }
+      const post = dbPostData.get({ plain: true });
+      res.render("edit-post", { post, loggedIn: true });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// redirect to sign in once signed up
+router.get("/new", (req, res) => {
+  res.render("new-post");
+});
+
+module.exports = router;
