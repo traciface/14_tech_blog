060a14a97a478172332d5cde77a09b89c9a88fe4
Traci <traci.face@gmail.com>
2021-05-31 20:44:57 -0400

    added post and login js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3006;
 
 const hbs = exphbs.create({ helpers });
 
@@ -25,7 +25,6 @@ const sess = {
 
 app.use(session(sess));
 
-// Inform Express.js on which template engine to use
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
