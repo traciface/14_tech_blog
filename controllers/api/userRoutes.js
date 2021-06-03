345d9a408255607fe247b6fad02a142b24d7ecfd
Traci <traci.face@gmail.com>
2021-06-02 21:52:03 -0400

    fixed errors and typos

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -59,6 +59,16 @@ router.post("/login", (req, res) => {
       });
   });
 
+  router.post("/logout", (req, res) => {
+    if (req.session.loggedIn) {
+      req.session.destroy(() => {
+        res.status(204).end();
+      });
+    } else {
+      res.status(404).end();
+    }
+  });
+  
 // R- gets the API users
 router.get("/", (req, res) => {
   User.findAll({
